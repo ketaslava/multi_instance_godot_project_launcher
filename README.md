87f0b1fc8335e7b08622b2a772ec498fe77146b7
KetaslavaKet <ketaslava@gmail.com>
2025-03-11 20:10:31 -0700

    init

---
+++ README.md
@@ -0,0 +1,52 @@
+# MIGPL
+
+Multi instance Godot project launcher
+
+# About
+
+MIGPL is a script for automating the launch of a game project on the Godot Engine project in multiple instances. The script allows you to significantly speed up development of multiplayer projects by allowing you to automatically launch the server and multiple clients without having to launch them manually every time you build a project. This script fully automates this task.
+
+# Features
+
+* Automatically run multiple instances of a project
+* Output debugging information to the console
+* Easy installation and use
+
+# Creators and contacts
+
+The script was developed by Ketaslava Ket and KTVINCCO Team
+
+[Contact Ketaslava Ket](http://ktvincco.com/ketaslava/contacts/)
+[Copyright Declaration](http://ktvincco.com/copyrightdeclaration/)
+
+# Instruction
+
+1. Download or clone repository
+
+2. Extract migpl.zip and go to the master/migpl/ folder
+
+3. Open terminal as admin
+
+4. Use "pip install -r requirements.txt" to install all requirements from requirements.txt for python3
+
+5. Copy migpl.py to root directory of your project (where there is a project.godot file)
+
+6. Go to the https://godotengine.org/ , click download (once), scroll down and download Export Templates
+
+7. Extract Export Templates, and copy them to root directory of your project -> GodotProject/templates/*ExtractTemplatesFiles*
+
+8. Open migpl.py and set export_template_path = "templates/YourSystemName.template"
+
+9. Set project instances count (process_instances_count)
+
+10. Set start arguments for instances of project (array_process_arguments), start arguments can be readed from any project script, use OS.get_cmdline_args() to get arguments array
+
+11. If you want the client window to be on top after launching for convenience, set it to launch last in the queue and set a delay of 0.5 seconds before it opens
+
+12. For convenience, you can remove the hotkeys, such as f3, f4 or f1, f2, in the shortcuts settings of the Godot editor and/or change script hotkeys settings
+
+13. Open terminal in the project root folder as admin or use sudo
+
+14. Run "python3 migpl.py"
+
+15. Read instruction, and press hotkey buttons to launch and stop project instances
---
+++ src/multi_instance_godot_project_launcher.py
@@ -0,0 +1,107 @@
+import subprocess
+import keyboard
+import time
+
+# ======= SETTINGS ======= #
+
+export_template_path = "templates/linux_debug.x86_64"
+
+key_run = 'f3'
+key_stop = 'f4'
+is_enable_auto_start = False
+
+process_instances_count = 3
+array_process_arguments = [
+    ["Arg1", "Arg2"],  # First instance arguments
+    ["Arg1", "Arg2", "Arg3"],  # Second instance arguments
+    ["Arg1"],  # Third instance arguments
+]
+array_process_delays = [0.0, 0.0, 0.5]  # Delay before start process to start on top of others
+
+# ======= ======== ======= #
+
+processes = []
+new_command = []
+
+
+def processes_run(queue_position):
+    global processes, export_template_path, new_command
+    # Combine command
+    args = []
+    new_command = []
+    if not queue_position >= len(array_process_arguments):
+        if isinstance(array_process_arguments[queue_position], list):
+            args = array_process_arguments[queue_position]
+    new_command.append(export_template_path)
+    for arg in args:
+        new_command.append(str(arg))
+    # Delay before start
+    delay = 0.0
+    if not queue_position >= len(array_process_delays):
+        if isinstance(array_process_delays[queue_position], float):
+            delay = array_process_delays[queue_position]
+    time.sleep(delay)
+    # Run process
+    print(">> Run command array " + str(new_command) + " \n")
+    processes.append(subprocess.Popen(new_command))
+    pass
+
+
+def processes_stop():
+    global processes
+    if len(processes) > 0:
+        process = processes[len(processes) - 1]
+        process.terminate()
+        processes.remove(process)
+
+
+def run_all():
+    print(">> Run all \n")
+    i = 0
+    while i < process_instances_count:
+        processes_run(i)
+        i += 1
+
+
+def stop_all():
+    global processes
+    i = 0
+    start_len = len(processes)
+    while i < start_len:
+        processes_stop()
+        i += 1
+    print(">> Stop all \n")
+
+
+def on_key_press(event):
+    if event.name == key_run:
+        run_all()
+    if event.name == key_stop:
+        stop_all()
+
+
+def main():
+    keyboard.on_press(on_key_press)
+    print(f"""\n\n
+>> Wellcome to MIGPL <<
+multi instance godot project launcher
+from Ketaslava Ket and KTVINCCO
+http://ktvincco.com/keta/contacts/
+https://github.com/ketaslava/migpl
+
+instrunction:
+* Press {key_run} to run processes
+* Press {key_stop} to stop all processes
+* Is enable auto start: {is_enable_auto_start}
+* Change other parameters in the script settings
+\n""")
+    # Auto start
+    if is_enable_auto_start:
+        run_all()
+    # Loop
+    while True:
+        pass
+
+
+if __name__ == '__main__':
+    main()
